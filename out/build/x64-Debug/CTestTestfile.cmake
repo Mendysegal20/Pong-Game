a0a83968989d4deb4692f144ba0494fdadc4aaec
Mendy <Mendysegal490@gmail.com>
2025-09-15 21:51:29 +0300

    Remove build folders and vcxproj from repo

--- out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/mendy/source/repos/PongGame/PongGame
-# Build directory: C:/Users/mendy/source/repos/PongGame/PongGame/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(PongTests "C:/Users/mendy/source/repos/PongGame/PongGame/out/build/x64-Debug/PongGameTests.exe")
-set_tests_properties(PongTests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/mendy/source/repos/PongGame/PongGame/CMakeLists.txt;58;add_test;C:/Users/mendy/source/repos/PongGame/PongGame/CMakeLists.txt;0;")
